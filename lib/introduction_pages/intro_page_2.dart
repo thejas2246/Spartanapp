a439bacc63e9ba5e9a13f0f7cae78a4cab319ac6
Thejas <thejas2246@gmail.com>
2025-01-11 21:58:45 +0530

    Text

--- lib/introduction_pages/intro_page_2.dart
+++ lib/introduction_pages/intro_page_2.dart
@@ -8,16 +8,22 @@ class IntroPage2 extends StatelessWidget {
     return Container(
       color: const Color.fromARGB(255, 255, 255, 255),
       child: Padding(
-        padding: const EdgeInsets.fromLTRB(0,100,0,0),
+        padding: const EdgeInsets.fromLTRB(0, 100, 0, 0),
         child: Column(
           children: [
-            Text("Discover Local Sports \n Communities",textAlign: TextAlign.center,style: TextStyle(fontSize: 30),),
-            Text("Join games or organize your own events.",textAlign: TextAlign.center,style: TextStyle(
-              fontSize: 15
-            ),),
+            Text(
+              "Discover Local Sports \n Communities",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Join games or organize your own events.",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 15),
+            ),
             SizedBox(
-             height: MediaQuery.sizeOf(context).height * 0.55,
-              ),
+              height: MediaQuery.sizeOf(context).height * 0.55,
+            ),
             Text(
               "Find games, tournaments, and teammates to stay \nactive and grow in your sports community",
               textAlign: TextAlign.center,
